b1df776a397ad7ac258fa190b99e5aeac11300d8
Soydaner Ulker <5871105+soydaner@users.noreply.github.com>
2023-10-02 13:37:35 +0100

    testing

--- app.js
+++ app.js
@@ -2,18 +2,22 @@
 const express = require('express');
 const app = express();
 
+
+
 // Getting Request
 app.get('/', (req, res) => {
 
+	if(process.env.NODE_ENV=='production')
+		res.send('You need to provide super secret password!')
+	else
 	// Sending the response
-	res.send('Hello CDDO CMS prototype work!')
-	
+		res.send('Hello CDDO CMS prototype work!')
 	// Ending the response
 	res.end()
 })
 
 // Establishing the port
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 // Executing the server on given port number
 app.listen(PORT, console.log(
